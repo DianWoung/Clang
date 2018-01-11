484885e320c449680169aa27d81cf038309caf3f
dianwang <dian126@foxmail.com>
2018-01-11 20:30:05 +0800

    pointer and string

--- pointer-and-string/main.c
+++ pointer-and-string/main.c
@@ -8,6 +8,15 @@ void show_info(char str[])
     printf("--------------------\n");
 }
 
+size_t str_length(const char *s)
+{
+    size_t len = 0;
+
+    while (*s++)
+        len ++;
+    return (len);
+}
+
 void show_double_info(double str[])
 {
     for (int i = 0; str[i]; i++)
@@ -15,22 +24,46 @@ void show_double_info(double str[])
     printf("--------------------\n");
     printf("--------------------\n");
 }
+
+char *str_copy(char *d, const char *s)
+{
+    char *t = d;
+    while (*d++ = *s++)
+    ;
+    return (t);
+}
+
 int main(void)
 {
     int i;
     char str[] = "ABC";
+    char s[] = "dianwang";
     char *ptr  = "123";
     double vc[] = {10, 20, 30, 40, 50};
     double *lptr = &vc[0];
+    char *pt[3] = {"linux", "Ubuntu", "docker"};
+    char gstr[3][6] = {"dian", "wang", "126"};
 
-    show_info(str);
+    show_info(gstr[0]);
+    show_info(gstr[1]);
+    show_info(gstr[2]);
+
+     str_copy(ptr, str);
+
+     show_info(ptr);
+     printf("str sizeof is %d\n",sizeof(str));
+    // show_info(ptr);  
+    // ptr = "E2EES";
+    // show_info(ptr);
+
+    // show_double_info(vc);
+
+     for (i = 0; i < 3; i++)
+        printf("%d ==> %s ==> %p\n", i, pt[i], &pt[i]);
+
+    printf("sting dianwang's length is %u.\n", (unsigned)str_length(s));
 
 
-    show_info(ptr);
-    
-    ptr = "E2EES";
-    show_info(ptr);
 
-    show_double_info(vc);
     return (0);
 }
